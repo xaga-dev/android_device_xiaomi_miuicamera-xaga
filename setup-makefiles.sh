c62c05dafa0a6d2638ff55374639804da65c7470
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:58:31 -0500

    Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -23,6 +23,12 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"vendor/xiaomi/miuicamera-xaga",
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
