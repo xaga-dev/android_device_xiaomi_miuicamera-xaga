a0fca4e68c4daec1b90acd7355b46bc43d699b0b
Giovanni Ricca <giovanniricca@proton.me>
2024-07-22 15:23:18 +0200

    Pull blobs from xaga V816.0.2.0.ULOCNXM
    * Pinned MiuiCamera source: MiuiCamera-smali / xaga/master
    
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Ibf5bcbf9dc4b55d89e7ad7dae25d3646b13db142

--- vendor/vendor-vendor.mk
+++ vendor/vendor-vendor.mk
@@ -7,9 +7,11 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/libcamera_algoup_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcamera_algoup_jni.xiaomi.so \
+    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/libcamera_ispinterface_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcamera_ispinterface_jni.xiaomi.so \
     vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/libcamera_mianode_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcamera_mianode_jni.xiaomi.so \
-    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/libmicampostproc_client.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmicampostproc_client.so \
-    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/vendor.xiaomi.hardware.campostproc@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.xiaomi.hardware.campostproc@1.0.so
+    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/libmtkisp_metadata_sys.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmtkisp_metadata_sys.so \
+    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so \
+    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/vendor.mediatek.hardware.camera.isphal@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.mediatek.hardware.camera.isphal@1.0.so
 
 PRODUCT_PACKAGES += \
     MiuiCamera
