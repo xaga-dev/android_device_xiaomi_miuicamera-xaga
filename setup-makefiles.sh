xiaomi-mediatek-devs/android_device_xiaomi_miuicamera-rubyx

Append `_system` to conflicting system libs
Change-Id: I74306c87e12079204223caccc0c77eacd4e01ec0

setup-makefiles.sh
@@ -23,6 +23,27 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "system_ext" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        vendor.mediatek.hardware.camera.isphal@1.0)
+            echo "${1}_system"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 

xaga-dev/android_device_xiaomi_miuicamera-xaga

setup-makefiles.sh
@@ -31,6 +31,27 @@ function vendor_imports() {
 EOF
 }
 
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "system" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        vendor.mediatek.hardware.camera.isphal@1.0)
+            echo "${1}_system"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 