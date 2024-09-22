fbe6a4da7f35a12447e09d8fd03d56a968765f26
Giovanni Ricca <giovanniricca@proton.me>
2024-09-22 09:24:17 +0200

    Regen vendor
    Change-Id: I9783aad08e48b368436368550149c2c263321d04

--- vendor/vendor-vendor.mk
+++ vendor/vendor-vendor.mk
@@ -5,13 +5,12 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/miuicamera-xaga/vendor
 
-PRODUCT_COPY_FILES += \
-    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/libcamera_algoup_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcamera_algoup_jni.xiaomi.so \
-    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/libcamera_ispinterface_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcamera_ispinterface_jni.xiaomi.so \
-    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/libcamera_mianode_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcamera_mianode_jni.xiaomi.so \
-    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/libmtkisp_metadata_sys.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmtkisp_metadata_sys.so \
-    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so \
-    vendor/xiaomi/miuicamera-xaga/vendor/proprietary/system/lib64/vendor.mediatek.hardware.camera.isphal@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.mediatek.hardware.camera.isphal@1.0.so
 
 PRODUCT_PACKAGES += \
+    libcamera_algoup_jni.xiaomi \
+    libcamera_ispinterface_jni.xiaomi \
+    libcamera_mianode_jni.xiaomi \
+    libmtkisp_metadata_sys \
+    vendor.mediatek.hardware.camera.isphal-V1-ndk \
+    vendor.mediatek.hardware.camera.isphal@1.0_system \
     MiuiCamera
